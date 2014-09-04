10118bb393cebbe1b423048e5a924670541c65d7
ZioCrocifisso <ziocroc@hotmail.it>
2014-09-04 22:45:26 +0200

    Updated example.

--- examples/basic/Main.hs
+++ examples/basic/Main.hs
@@ -5,28 +5,44 @@ module Main where
 import Monkey
 
 import Data.Fixed (mod')
+import Data.List (unfoldr)
 import FRP.Yampa
 import FWGL
+
+mouseCount :: Num a => a -> MouseButton -> SF Input a
+mouseCount i mb = mouse mb >>> accumHoldBy (\c _ -> c + 1) i
+
+gradient :: Color -> Color -> Texture
+gradient c1 c2 = mkTexture (floor w) (floor w) $ unfoldr f (0, 0)
+        where cerp (Color r g b a) (Color r' g' b' a') t =
+                Color (lerp r r' t) (lerp g g' t) (lerp b b' t) (lerp a a' t)
+              lerp v v' t = floor $ (1 - t) * (fromIntegral v)
+                                  + t * (fromIntegral v')
+              swing x mx = if x >= mx / 2 then mx - x else x
+              f (x, y) | x == w = f (0 :: Float, y + 1)
+                       | y == w = Nothing
+                       | otherwise = Just $ ( cerp c1 c2 . (/ (w / 2)) $
+                                                swing x w + swing y w
+                                            , (x + 1, y) )
+              w = 256 :: Num a => a
 
 mainSF :: SF Input (Scene, Audio)
 mainSF = proc inp -> do (x, y) <- pointer -< inp
 
                         tm <- time >>> arr realToFrac -< inp
+                        leftCount <- mouseCount 4 MouseLeft -< inp
+                        rightCount <- mouseCount 0 MouseRight -< inp
 
-                        mLeft <- mouse MouseLeft -< inp
-                        leftCount <- accumHoldBy (\c _ -> c + 1) 1 -< mLeft
-
-                        mRight <- mouse MouseRight -< inp
-                        rightCount <- accumHoldBy (\c _ -> c + 1) 0 -< mRight
-
-                        let scaleFact = (leftCount - rightCount) / 3
-                            transformedMonkey = rotX (fromIntegral y / 120 - 1) $
+                        let scaleFact = (leftCount - rightCount) / 20
+                            transformedMonkey = color (visible 150 50 0) $
+                                                rotX (fromIntegral y / 120 - 1) $
                                                 rotY (fromIntegral x / 160 + 1) $
                                                 scale scaleFact $
                                                 geom monkey
-                            transformedCube o = rotX (mod' ((tm + o) / 100) $
+                            transformedCube o = texture (gradient yellow red) $
+                                                rotX (mod' ((tm + o) / 200) $
                                                            (pi * 2)) $
-                                                rotY (mod' ((tm + o) / 200) $
+                                                rotY (mod' ((tm + o) / 400) $
                                                            (pi * 2)) $
                                                 translate (V3 9 0 0) $
                                                 rotZ (mod' ((tm + o) / 400)
